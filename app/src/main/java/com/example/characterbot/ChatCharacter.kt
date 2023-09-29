4250717ec31e5d482d9ed9195edda57451fc35a3
fauxriarty <aditya.chheda04@gmail.comgith>
2023-09-30 00:37:27 +0530

    made a v basic layout, tried to explore some functions

---
+++ app/src/main/java/com/example/characterbot/ChatCharacter.kt
@@ -0,0 +1,3 @@
+package com.example.characterbot
+
+data class ChatCharacter(val name: String, val style: Int)
--- app/src/main/java/com/example/characterbot/MainActivity.kt
+++ app/src/main/java/com/example/characterbot/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.characterbot
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import android.text.Html
+import android.widget.ArrayAdapter
+import com.example.characterbot.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val characters = listOf(
+            ChatCharacter("Nick", R.style.Nick),
+            ChatCharacter("Mike", R.style.Mike)
+            // Add more characters as needed
+        )
+
+        val characterSpinner = binding.characterSpinner
+        val characterAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, characters.map { it.name })
+        characterAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        characterSpinner.adapter = characterAdapter
+
+        val chatText = binding.chatText
+        val chatInput = binding.chatInput
+        val sendButton = binding.sendButton
+
+        sendButton.setOnClickListener {
+            val message = chatInput.text.toString()
+            if (message.isNotEmpty()) {
+                val character = characters[characterSpinner.selectedItemPosition]
+                val styledMessage = "<span class=\"${resources.getResourceEntryName(character.style)}\">$message</span>"
+                chatText.append(Html.fromHtml(styledMessage, Html.FROM_HTML_MODE_LEGACY))
+                chatText.append("\n")
+                chatInput.text.clear()
+            }
+        }
     }
+
 }
+
