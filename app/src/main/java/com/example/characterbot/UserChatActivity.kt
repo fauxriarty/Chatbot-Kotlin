0cae958ec3fee00879bb782b2d2a74c3bf971e69
fauxriarty <aditya.chheda04@gmail.comgith>
2023-10-12 00:15:18 +0530

    improved layouts

--- app/src/main/java/com/example/characterbot/UserChatActivity.kt
+++ app/src/main/java/com/example/characterbot/UserChatActivity.kt
@@ -3,6 +3,8 @@ package com.example.characterbot
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import com.example.characterbot.databinding.ActivityUserChatBinding
@@ -37,6 +39,10 @@ class UserChatActivity : AppCompatActivity() {
         toggle.syncState()
         toggle.isDrawerIndicatorEnabled = false
 
+        val usersList = listOf("Alice", "Bob", "Charlie", "David", "Eva")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, usersList)
+        binding.usersListView.adapter = adapter
+
         binding.navigationView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.chatWithBot -> {
@@ -44,10 +50,12 @@ class UserChatActivity : AppCompatActivity() {
                     startActivity(intent)
                     true
                 }
+
                 R.id.chatWithUsers -> {
                     drawerLayout.closeDrawer(GravityCompat.START)
                     true  // it's the current page, so no action is needed
                 }
+
                 else -> false
             }
         }
@@ -60,9 +68,18 @@ class UserChatActivity : AppCompatActivity() {
             }
         }
 
+        binding.usersListView.setOnItemClickListener { _, _, position, _ ->
+            val selectedUser = usersList[position]
+            // For now, it will just display a Toast. But you can navigate to the chat page here.
+            // val intent = Intent(this, ChatScreenActivity::class.java).apply {
+            //     putExtra("selectedUser", selectedUser)
+            // }
+            // startActivity(intent)
+            Toast.makeText(this, "Selected User: $selectedUser", Toast.LENGTH_SHORT).show()
+
+        }
+
     }
-
-
 }
 
 
